b893553289f4ef5dc445e5f9f2d7c0dfda287006
RishabTR <rishab.official.8@example.com>
2023-12-29 00:59:22 +0530

    is it success

---
+++ html/script.js
@@ -0,0 +1,8 @@
+function boo(p){
+    
+    // document.getElementById('sample').innerHTML ='HELLO BOO1';
+    // document.getElementById('sample').innerHTML ='HELLO BOO2';
+    // document.getElementById('sample').innerHTML ='HELLO BOO3';
+    // document.getElementById('sample').innerHTML ='HELLO BOO4';
+    alert(p.NAME)
+}
